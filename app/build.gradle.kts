3abdc9e8fbc43434881a627af45c62beb878050c
WaffleMaker9000 <tomkrnt@gmail.com>
2025-05-14 22:22:11 +0200

    Actually implemented a main menu screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.minovepole"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.minovepole"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
--- app/src/main/java/com/example/minovepole/ui/theme/Color.kt
+++ app/src/main/java/com/example/minovepole/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.theme
+package com.example.minovepole.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
