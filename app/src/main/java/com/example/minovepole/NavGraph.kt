6377e26723cfdb5192a5caec53244bab288f59b2
WaffleMaker9000 <tomkrnt@gmail.com>
2025-05-15 03:26:29 +0200

    Fixed state remembering in NavGraph

--- app/src/main/java/com/example/minovepole/NavGraph.kt
+++ app/src/main/java/com/example/minovepole/NavGraph.kt
@@ -1,6 +1,10 @@
 package com.example.minovepole
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -8,6 +12,9 @@ import androidx.navigation.compose.composable
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
+    var mineDifficultySelected by remember { mutableStateOf(DifficultyOption.MEDIUM) }
+    var sizeDifficultySelected by remember { mutableStateOf(DifficultyOption.MEDIUM) }
+
     NavHost(navController = navController, startDestination = "main_menu") {
         composable("main_menu") {
             MainMenu(
@@ -19,10 +26,10 @@ fun AppNavGraph(navController: NavHostController) {
 
         composable("difficulty") {
             DifficultyScreen(
-                mineDifficultySelected = DifficultyOption.MEDIUM,
-                onMineDifficultySelected = {},
-                sizeDifficultySelected = DifficultyOption.MEDIUM,
-                onSizeDifficultySelected = {},
+                mineDifficultySelected = mineDifficultySelected,
+                onMineDifficultySelected = { mineDifficultySelected = it },
+                sizeDifficultySelected = sizeDifficultySelected,
+                onSizeDifficultySelected = { sizeDifficultySelected = it},
                 onConfirm = {}
             )
         }
